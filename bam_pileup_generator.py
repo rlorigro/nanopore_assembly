317e3cd564845ee53affa26abfd49a9bb71f3ae7
ryan <rlorigro@ucsc.edu>
2018-08-09 16:23:07 -0700

    Working partial order of BAM-derived aligned read segments

---
+++ bam_pileup_generator.py
@@ -0,0 +1,98 @@
+from handlers.BamHandler import BamHandler
+from handlers.FastaHandler import FastaHandler
+from handlers.VcfHandler import VCFFileProcessor
+from modules.PileupGenerator import PileupGenerator
+from poapy import poagraph
+from poapy import seqgraphalignment
+
+
+def print_segments(ref_sequence, sequences):
+    print(ref_sequence)
+    print("-" * len(ref_sequence))
+    for sequence in sequences:
+        print(sequence)
+    print()
+
+if __name__ == "__main__":
+    chromosome_name = "18"
+
+    # ---- GIAB (dev machine) -------------------------------------------------
+    # bam_file_path = "/home/ryan/data/GIAB/NA12878_GIAB_30x_GRCh37.sorted.bam"
+    # reference_file_path = "/home/ryan/data/GIAB/GRCh37_WG.fa"
+    # vcf_path = "/home/ryan/data/GIAB/NA12878_GRCh37.vcf.gz"
+
+    # ---- Nanopore GUPPY (dev machine) --------------------------------------
+    bam_file_path = "/home/ryan/data/Nanopore/BAM/Guppy/rel5-guppy-0.3.0-chunk10k.sorted.bam"
+    reference_file_path = "/home/ryan/data/GIAB/GRCh38_WG.fa"
+    vcf_path = "/home/ryan/data/GIAB/NA12878_GRCh38_PG.vcf.gz"
+    bed_path = "/home/ryan/data/GIAB/NA12878_GRCh38_confident.bed"
+
+    chromosome_name = "chr" + chromosome_name
+    # -------------------------------------------------------------------------
+
+    start_position = 1000000
+    end_position = 19000000
+
+    bam_handler = BamHandler(bam_file_path)
+    fasta_handler = FastaHandler(reference_file_path)
+    vcf_handler = VCFFileProcessor(vcf_path)
+
+    vcf_handler.populate_dictionary(contig=chromosome_name,
+                                    start_pos=start_position,
+                                    end_pos=end_position+1)
+
+    positional_variants = vcf_handler.get_variant_dictionary()
+
+    for p,position in enumerate(positional_variants):
+        pileup_start = position - 5
+        pileup_end = position + 5      # add random variation here
+
+        print(pileup_start, pileup_end)
+
+        ref_sequence = fasta_handler.get_sequence(chromosome_name=chromosome_name,
+                                                  start=pileup_start,
+                                                  stop=pileup_end + 1)
+
+        reads = bam_handler.get_reads(chromosome_name=chromosome_name,
+                                      start=pileup_start,
+                                      stop=pileup_end)
+
+        pileup_generator = PileupGenerator(chromosome_name=chromosome_name,
+                                           start_position=pileup_start,
+                                           end_position=pileup_end,
+                                           ref_sequence=ref_sequence,
+                                           reads=reads)
+
+        sequence_dictionary = pileup_generator.get_read_segments()
+        read_ids, sequences = zip(*sequence_dictionary.items())
+
+        print_segments(ref_sequence, sequences)
+
+        graph = poagraph.POAGraph(ref_sequence, "ref")
+        for i in range(len(sequences)):
+            sequence = sequences[i]
+            read_id = read_ids[i]
+
+            # print("sequence\t", sequence)
+            # print("label\t\t", read_id)
+            #
+            # print("length\t\t", len(sequence))
+
+            alignment = seqgraphalignment.SeqGraphAlignment(sequence, graph,
+                                                            fastMethod=True,
+                                                            globalAlign=True,
+                                                            matchscore=1,
+                                                            mismatchscore=-1,
+                                                            gapscore=-2)
+
+            # print(alignment)
+
+            graph.incorporateSeqAlignment(alignment, sequence, str(i))
+
+        alignments = graph.generateAlignmentStrings()
+
+        for read_id, alignstring in alignments:
+            print("{0:15s} {1:s}".format(read_id, alignstring))
+
+        if p > 1:
+            exit()
